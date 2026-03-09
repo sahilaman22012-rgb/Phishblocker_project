91416d7562e28ffc9483d9e4612572f8e541ed74
sahilaman22012-rgb <sahilaman22012@gmail.com>
2026-03-09 22:09:02 +0530

    Update loader.py

--- backend/loader.py
+++ backend/loader.py
@@ -6,5 +6,5 @@ DATA_PATH = "data/dataset.csv"
 if not os.path.exists(DATA_PATH):
     os.makedirs("data", exist_ok=True)
 
-    url = "https://drive.google.com/file/d/1WSYi4g-oY36YRVmQRNDhliGhDPPrRf_7/view?usp=drive_link"
+    url = "https://drive.google.com/uc?id=1WSYi4g-oY36YRVmQRNDhliGhDPPrRf_7"
     gdown.download(url, DATA_PATH, quiet=False)
