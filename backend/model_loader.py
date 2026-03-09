c19a90260baa98d6cd66b10d8902eb6189c1003c
sahilaman22012-rgb <sahilaman22012@gmail.com>
2026-03-09 21:20:03 +0530

    Update model download URL for direct access

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -6,6 +6,6 @@ MODEL_PATH = "phish_url_model.joblib"
 if not os.path.exists(MODEL_PATH):
     print("Downloading ML model...")
     
-    url = "https://drive.google.com/file/d/1rMNUuve1gC-bjZfLIElQ7b2m8VAq6XWm/view?usp=drive_link"
+    url = "https://drive.google.com/uc?id=1rMNUuve1gC-bjZfLIElQ7b2m8VAq6XWm"
     
     gdown.download(url, MODEL_PATH, quiet=False)
