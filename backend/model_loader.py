a9544ce705d591a5c33b077855905151bb3f98e4
sahilaman22012-rgb <sahilaman22012@gmail.com>
2026-03-09 22:11:42 +0530

    Update model_loader.py

--- backend/model_loader.py
+++ backend/model_loader.py
@@ -6,6 +6,6 @@ MODEL_PATH = "phish_url_model.joblib"
 if not os.path.exists(MODEL_PATH):
     print("Downloading ML model...")
     
-    url = "https://drive.google.com/uc?id=1rMNUuve1gC-bjZfLIElQ7b2m8VAq6XWm"
+    url = "https://drive.google.com/uc?export=download&id=1rMNUuve1gC-bjZfLIElQ7b2m8VAq6XWm"
     
     gdown.download(url, MODEL_PATH, quiet=False)
